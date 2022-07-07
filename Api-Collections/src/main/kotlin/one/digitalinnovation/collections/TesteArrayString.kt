ab36f09a3a97a1114abf9c8b548a5b1f8b3685e1
edubarbosa <eduardobabrosalhp@gmail.com>
2022-07-07 16:48:18 -0300

    Organizando arquivos de array Int e String

--- Api-Collections/src/main/kotlin/one/digitalinnovation/collections/TesteArrayString.kt
+++ Api-Collections/src/main/kotlin/one/digitalinnovation/collections/TesteArrayString.kt
@@ -1,6 +1,8 @@
 package one.digitalinnovation.collections
 
 fun main() {
+
+    // Array de string comum (mais verboso)
     val nomes = Array(3){""}
     nomes[0] = "Maria"
     nomes[1] = "Carlos"
@@ -10,4 +12,16 @@ fun main() {
         println(nome)
     }
 
+
+    println("-----------------")
+
+
+
+    // Array de string (dinâmico) com tipo inferido usando arrayOf
+    val nomes2 = arrayOf("Maria", "José", "Carlos")
+
+    nomes2.sort()
+    nomes2.forEach {
+        println(it)
+    }
 }
--- Api-Collections/src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
+++ Api-Collections/src/main/kotlin/one/digitalinnovation/collections/TesteIntArray.kt
@@ -1,6 +1,8 @@
 package one.digitalinnovation.collections
 
 fun main() {
+
+    // Array de Int mais verboso
     val values = IntArray(5)
 
     values[0] = 5
@@ -9,15 +11,15 @@ fun main() {
     values[3] = 6
     values[4] = 8
 
-    for (valor in values){
-        println(valor)
-    }
+        for (valor in values){
+            println(valor)
+        }
 
     println("-----------------------")
 
-    for (index in values.indices){
-        println(values[index])
-    }
+        for (index in values.indices){
+            println(values[index])
+        }
 
     println("-----------------------")
 
@@ -31,6 +33,17 @@ fun main() {
     // Sort para ordernar a lista
     values.sort()
     for (valor in values){
+        println(valor)
+    }
+
+
+    println("-----------------------")
+
+
+    // Array dinâmico, com tamanho inferido de acordo com os valores dentro dele, sem tamanho pré-definido.
+    val numeros = intArrayOf(5, 8, 0, 2, 6, 15)
+
+    for (valor in numeros){
         println(valor)
     }
 
